27751297d362b79fce249d13e1596452b3abf71e
MusabekovD <diyormusabekov1407@gmail.com>
2024-10-14 13:32:24 +0500

    10th lesson 15:41

--- public_html/core/base/controller/BaseController.php
+++ public_html/core/base/controller/BaseController.php
@@ -6,6 +6,8 @@ use core\base\exception\RouteException;
 
 abstract class BaseController
 {
+    protected $page;
+    protected $errors;
     protected $controller;
     protected $InputMethod;
     protected $OutputMethod;
@@ -24,7 +26,33 @@ abstract class BaseController
             ];
             $object->invoke(new $controller, $args);
         } catch (\ReflectionException $e) {
-            throw new RouteException($e);
+            throw new RouteException($e->getMessage());
         }
+
+        public funciton request($args){
+            $this->parameters = $args["parameters"];
+
+            $inputData = $args["inputData"];
+            $outputData = $args["outputData"];
+
+            $this->inputData();
+
+            $this->page = $this->outputData();
+
+            if($this->errors){
+                $this->writelog();
+            }
+            $this->getPage();
+        }
+        protected function render($path = "", $parameters = []){
+                extract($parameters);
+                if(!path){
+                    $path = TEMPLATE . explode("controller", strtolower( (new \ReflectionClass($this))->getShortName()))[0];
+                }
+        }
+        protected function getPage(){
+            exit($this->page);
+        }
+
     }
 }
